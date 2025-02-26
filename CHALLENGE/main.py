5b2ba1368cd6064beefc87d7ff8b9c58afaea26d
giovannilopopolo98 <33524900+giovannilopopolo98@users.noreply.github.com>
2025-02-26 20:49:49 +0100

    Update main.py

--- CHALLENGE/main.py
+++ CHALLENGE/main.py
@@ -5,7 +5,7 @@ from solver import Solver
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--lr", type=float, default=0.001)
-    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--batch_size", type=int, default=32)   
     parser.add_argument("--max_epochs", type=int, default=100)
     
     parser.add_argument("--ckpt_dir", type=str, default="D:/UNIVERSITA/MAGISTRALE/SECONDO_ANNO/PRIMO_SEMESTRE/Deep_Learning/Script/CHALLENGE/CHALLENGE/checkpoint")
@@ -15,8 +15,8 @@ def main():
     parser.add_argument("--data_dir", type=str,
                         default=os.path.join("D:\\", "UNIVERSITA", "MAGISTRALE", "SECONDO_ANNO", "PRIMO_SEMESTRE", "Deep_Learning", "Script", "CHALLENGE", "DepthEstimationUnreal"))
 
-    parser.add_argument("--is_train", type=bool, default=True)
-    parser.add_argument("--ckpt_file", type=str, default="depth_10.pth")
+    parser.add_argument("--is_train", type=bool, default=False)
+    parser.add_argument("--ckpt_file", type=str, default="D:/UNIVERSITA/MAGISTRALE/SECONDO_ANNO/PRIMO_SEMESTRE/Deep_Learning/Script/CHALLENGE/CHALLENGE/checkpoint/depth_v_0.4.1_196.pth")
 
     args = parser.parse_args()
     solver = Solver(args)
